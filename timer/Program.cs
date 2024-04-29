d7ebb5dac840302b9c158a6b9a1a04af8d5c3cb3
Christopher Schmidt <schmidtc086@outlook.com>
2024-04-28 22:33:36 -0400

    Initial commit with basic structure

---
+++ timer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Timers;
+using System.Media;
+using System.Diagnostics;
+using System.ComponentModel;
+
+namespace Reminder {
+    class Reminder {
+        static void Main(string[] args)
+        {
+            System.Timers.Timer timer = new System.Timers.Timer(2000);
+            Stopwatch stopwatch = new Stopwatch();
+
+            const int minutes = 6;
+            const int secondsInMinutes = 60;
+            const int msInSeconds = 1000;
+
+            timer.Elapsed += OnTimedEvent;
+            timer.Interval = minutes * secondsInMinutes * msInSeconds;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+            stopwatch.Start();
+
+            
+
+            Console.WriteLine("Press \'q\' to quit the timer.");
+            while (Console.Read() != 'q');
+            
+        }
+        private static void OnTimedEvent(object? source, ElapsedEventArgs e) {
+            Console.WriteLine("Hello World!");
+            #pragma warning disable CA1416 // Validate platform compatibility
+            SoundPlayer player = new SoundPlayer("beep-07a.wav");
+            player.Play();
+            #pragma warning restore CA1416 // Validate platform compatibility
+        }
+    }
+}
+
+
+
